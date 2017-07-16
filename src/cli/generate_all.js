f5849ede42d70919c26794d8cfca0a87a8f123c0
David Chiles <david@davidchiles.org>
2017-07-15 19:08:29 -0700

    Generate POI data from layout file

--- src/cli/generate_all.js
+++ src/cli/generate_all.js
@@ -1,3 +1,5 @@
+var turf = require('turf');
+var Geocoder = require('../geocoder/geocoder.js');
 var fs = require('fs')
   , Generate = require('../generate.js')
   , nopt = require("nopt")
@@ -13,9 +15,11 @@ var fs = require('fs')
 var directory = parsed.directory;
 var layoutPath = directory + "/layouts/layout.json"
 var toiletLayoutPath = directory + "/layouts/toilet.json"
+var poiLayoutPath = directory + "/layouts/poi.json"
 
 var layout = require(layoutPath);
 var toiletLayout = require(toiletLayoutPath);
+var poiLayout = require(poiLayoutPath);
 
 var outputDict = {
   "streets" : "/geo/streets.geojson",
@@ -25,6 +29,7 @@ var outputDict = {
   "dmz" : "/geo/dmz.geojson"
 };
 
+/** Generate Toilets GEOJSON */
 for (var key in outputDict) {
   var result = Generate.generate(key,layout);
   if (result) {
@@ -35,4 +40,19 @@ for (var key in outputDict) {
 var toiletResult = Generate.generateToilets(layout,toiletLayout);
 if (toiletResult) {
   fs.writeFile(directory + "/geo/toilets.geojson", JSON.stringify(toiletResult,null,4), function(err) {});
+}
+
+/** Generate POI GEOJSON*/
+var features = [];
+var geocoder = new Geocoder(layout);
+for (var index in poiLayout) {
+  var poi = poiLayout[index];
+  var point = geocoder.forwardTimeDistance(poi.address.time,poi.address.distance / 5280.0,'miles');
+  point.properties = poi.properties;
+  features.push(point);
+}
+
+var fc = turf.featureCollection(features);
+if (fc) {
+  fs.writeFile(directory + "/geo/points.geojson", JSON.stringify(fc,null,4), function(err) {});
 }
