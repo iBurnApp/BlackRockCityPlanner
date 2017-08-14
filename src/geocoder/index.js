96a8a52374b670fc016551e4fd7608c907cc4824
David Chiles <david@davidchiles.org>
2017-08-14 07:13:09 -0700

    Updated bundle.js for 2017

--- src/geocoder/index.js
+++ src/geocoder/index.js
@@ -1,5 +1,5 @@
 var Geocoder = require('./geocoder.js');
-var layoutFile = require('../../../data/2017/geo/layout.json');
+var layoutFile = require('../../../iBurn-Data/data/2017/layouts/layout.json');
 
 /**
  * This can take a few seconds to setup all the necessary geo files.
