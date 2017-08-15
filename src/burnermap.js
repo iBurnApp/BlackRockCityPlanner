3972b90a3a7e404319160235c35f4c8c188b455a
David Chiles <david@davidchiles.org>
2017-08-14 19:15:23 -0700

    Fixes for merging burner map

--- src/burnermap.js
+++ src/burnermap.js
@@ -1,4 +1,5 @@
 var Geocoder = require('./geocoder/geocoder.js');
+var fs = require('fs');
 
 var nopt = require("nopt")
   , path = require("path")
@@ -16,13 +17,15 @@ var nopt = require("nopt")
    }
   , parsed = nopt(knownOpts, shortHands, process.argv, 2)
 
-  var aJson = require(parsed.api);
+  // require() does something different depeding on the file extension (maybe?)
+  var aJson = JSON.parse(fs.readFileSync(parsed.api, 'utf8'));
   var layoutFile = require(parsed.file);
   var bJson = require(parsed.burnermap);
 
   var combineBurnerMap = function(layout,apiJSON,burnerJSON) {
     var geocoder = new Geocoder(layout);
     var burnermapDict = {};
+    //Geocode every burner map dict if it has a camp api key
     burnerJSON.camps.forEach(function(burnerCamp){
       if (burnerCamp.apiID.length > 0 && burnerCamp.adClock !== "?:??") {
         var geoLocation = geocoder.forward(burnerCamp.adClock,burnerCamp.adRing);
@@ -33,20 +36,25 @@ var nopt = require("nopt")
           location.gps_longitude = geoLocation.geometry.coordinates[0];
           location.gps_latitude = geoLocation.geometry.coordinates[1];
         } else {
-          console.log(burnerCamp.name+": "+burnerCamp.adClock + " & " + burnerCamp.adRing);
+          console.log("Failed: " + burnerCamp.name+": " + burnerCamp.adClock + " & " + burnerCamp.adRing);
         }
         burnermapDict[burnerCamp.apiID] = location;
       }
-      
-    });
+
+    })
+    // Add the location to the existing official API JSON
     return apiJSON.map(function(camp){
       var location = burnermapDict[camp.uid];
       if (location) {
-        camp.location = location
+        camp.burnermap_location = location
       }
       return camp;
     });
   }
 
 var result = combineBurnerMap(layoutFile,aJson,bJson);
-//console.log(JSON.stringify(result,null,4));
+if (parsed.out) {
+    fs.writeFile(parsed.out, JSON.stringify(result, null, 4), function(err) {});
+} else {
+    console.log(JSON.stringify(result));
+}
